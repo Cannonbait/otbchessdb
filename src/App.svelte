2eaa279d8aab5a2093a0e2c0154b42049450c914
Ivar Josefsson <ivar.josefsson@gmail.com>
2023-01-27 12:35:43 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  async function printToConsole() {
+    console.log(await file[0].text());
+  }
+  let file;
+  let textFile;
+  $: {
+    if (file != null) {
+      printToConsole();
+    }
+  }
+
+  async function doTheConsoleLog() {}
+</script>
+
+<main>
+  Select a pgn file
+  <input accept=".pgn" type="file" bind:files={file} />
+  {#if file}
+    <div>File selected</div>
+  {/if}
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
